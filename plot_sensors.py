bf5facedbffeee1be16617efbb2e479ed2065221
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2012-04-11 23:33:07 -0700

    Create a class for plotting timeseries data.

--- plot_sensors.py
+++ plot_sensors.py
@@ -4,42 +4,57 @@ import sys
 from PyQt4 import QtCore, QtGui
 import numpy as np
 import pyqtgraph as pg
+import array
+import argparse
+import random
 
-## create GUI
-app = QtGui.QApplication([])
-w = QtGui.QMainWindow()
-w.resize(800,600)
-v = pg.GraphicsView()
-#v.invertY(True)  ## Images usually have their Y-axis pointing downward
-v.setAspectLocked(True)
-v.enableMouse(True)
-v.autoPixelScale = False
-w.setCentralWidget(v)
-s = v.scene()
-#v.setRange(QtCore.QRect(-2, -2, 220, 220))
-w.show()
+class TimeSeriesPlotter():
+    def __init__(self, historySize=100):
+        self.data = {}
+        self.indices = {}
+        self.curves = {}
+        self.historySize = historySize
+        self.app = QtGui.QApplication([])
+        self.w = QtGui.QMainWindow()
+        self.w.resize(800,600)
+        self.v = pg.GraphicsView()
+        #v.invertY(True)  ## Images usually have their Y-axis pointing downward
+        self.v.setAspectLocked(True)
+        self.v.enableMouse(True)
+        self.v.autoPixelScale = False
+        self.w.setCentralWidget(self.v)
+        self.s = self.v.scene()
+        #v.setRange(QtCore.QRect(-2, -2, 220, 220))
+        self.w.show()
+        self.pi = pg.PlotItem()
+        self.s.addItem(self.pi)
+        self.pi.scale(1.0, 1.0)
+        self.pi.setGeometry(10, 10, 400, 300)
 
-pi1 = pg.PlotItem()
-r = abs(np.random.normal(loc=0, scale=100*0.1, size=100))
-curve = pg.PlotCurveItem()
-curve.updateData(r)
-pi1.addCurve(curve)
-s.addItem(pi1)
+    def addData(self, value, key=0):
+        if self.data.has_key(key):
+            idx = self.indices[key]
+            self.data[key][idx] = value
+            self.indices[key] = (idx + 1) % self.historySize # Ring buffering
+        else:
+            self.data[key] = np.zeros(self.historySize)
+            self.data[key][0] = value
+            self.indices[key] = 1
+            curve = pg.PlotCurveItem()
+            self.curves[key] = curve
+            self.pi.addCurve(curve)
+        self.curves[key].updateData(self.data[key])
 
-pi1.scale(1.0, 1.0)
-pi1.setGeometry(10, 10, 400, 300)
+    def addRandomData(self, key=0):
+        r = random.random()
+        self.addData(r, key)
 
-pi1.plot()
-
-def updateImage():
-    r = abs(np.random.normal(loc=0, scale=100*0.1, size=100))
-    curve.updateData(r)
-
+tsp = TimeSeriesPlotter()
 ## Rapidly update one of the images with random noise
 t = QtCore.QTimer()
-t.timeout.connect(updateImage)
+t.timeout.connect(tsp.addRandomData)
 t.start(50)
 
 ## Start Qt event loop unless running in interactive mode.
 if sys.flags.interactive != 1:
-    app.exec_()
+    tsp.app.exec_()
