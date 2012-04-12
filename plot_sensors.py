a23e63c1e56de042c87bde23d3a429b84dce770c
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2012-04-11 23:55:49 -0700

    Plots stuff.

--- plot_sensors.py
+++ plot_sensors.py
@@ -9,6 +9,10 @@ import argparse
 import random
 
 class TimeSeriesPlotter():
+    """
+    A class for plotting data over time. Uses a clever trick found on Stack Overflow and stores the data twice so that
+    it can easily get a view of the data by slicing, and uses the excellent PyQtGraph library by Luke Campagnola.
+    """
     def __init__(self, historySize=100):
         self.data = {}
         self.indices = {}
@@ -28,28 +32,28 @@ class TimeSeriesPlotter():
         self.w.show()
         self.pi = pg.PlotItem()
         self.s.addItem(self.pi)
-        self.pi.scale(1.0, 1.0)
-        self.pi.setGeometry(10, 10, 400, 300)
+        self.pi.scale(0.7, 0.7)
+        self.pi.setGeometry(10, 10, 800, 600)
+        self.lastval = 10
 
     def addData(self, value, key=0):
-        if self.data.has_key(key):
-            idx = self.indices[key]
-            self.data[key][idx] = value
-            self.indices[key] = (idx + 1) % self.historySize # Ring buffering
-        else:
-            self.data[key] = np.zeros(self.historySize)
-            self.data[key][0] = value
-            self.indices[key] = 1
-            curve = pg.PlotCurveItem()
-            self.curves[key] = curve
-            self.pi.addCurve(curve)
-        self.curves[key].updateData(self.data[key])
+        if not self.data.has_key(key):
+            self.data[key] = np.zeros(self.historySize*2)
+            self.indices[key] = 0
+            self.curves[key] = pg.PlotCurveItem()
+            self.pi.addCurve(self.curves[key])
+        idx = self.indices[key]
+        self.data[key][idx] = value
+        self.data[key][idx + self.historySize] = value
+        self.curves[key].updateData(self.data[key][idx+1 : idx+self.historySize]+1)
+        self.indices[key] = (idx + 1) % self.historySize # Ring buffering
 
     def addRandomData(self, key=0):
-        r = random.random()
+        r = random.gauss(self.lastval, 1)
         self.addData(r, key)
+        self.lastval = r
 
-tsp = TimeSeriesPlotter()
+tsp = TimeSeriesPlotter(50)
 ## Rapidly update one of the images with random noise
 t = QtCore.QTimer()
 t.timeout.connect(tsp.addRandomData)
